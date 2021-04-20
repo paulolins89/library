f20b1c1d1cdf26260e79d20d503f93fd0e5bc363
Paulo Lins <paulolins89@gmail.com>
2021-04-20 11:38:19 -0300

    gave popup form some functionality

--- library.js
+++ library.js
@@ -8,5 +8,17 @@ function Book(name, author, pages, read){
 }
 
 function addBookToLibrary(){
-    
+
 }
+
+const addButton = document.getElementById('addButton');
+const bookForm = document.getElementById('book-form');
+const closeForm = document.getElementById('closeButton');
+
+addButton.addEventListener('click', () => {
+    bookForm.style.display = "block";
+})
+
+closeForm.addEventListener('click', () =>{
+    bookForm.style.display = "none";
+})
